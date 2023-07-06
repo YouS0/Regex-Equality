adf4bff06ec955dfc518bae030cb5e9b7ba1f41f
Younes Soleimani <YouS0>
2023-07-06 10:39:11 +0330

    RegexTree and DFA added

--- Regex2NFA.py
+++ Regex2NFA.py
@@ -205,3 +205,85 @@ class RegexNode:
         print(str(level) + ' ' + self.item, self.firstpos, self.lastpos, self.nullable, '' if self.position == None else self.position)
         for child in self.children:
             child.write_level(level+1)
+
+class RegexTree:
+
+    def __init__(self, regex):
+        self.root = RegexNode(regex)
+        self.followpos = []
+        self.functions()
+    
+    def write(self):
+        self.root.write_level(0)
+
+    def functions(self):
+        positions = self.root.calc_functions(0, self.followpos)   
+        if DEBUG == True:
+            print(self.followpos)
+    
+    def toDfa(self):
+
+        def contains_hashtag(q):
+            for i in q:
+                if self.followpos[i][0] == '#':
+                    return True
+            return False
+
+        M = [] #Marked states
+        Q = [] #States list in the followpos form ( array of positions ) 
+        V = alphabet - {'#', lambda_symbol if use_lambda else ''} #Automata alphabet
+        d = [] #Delta function, an array of dictionaries d[q] = {x1:q1, x2:q2 ..} where d(q,x1) = q1, d(q,x2) = q2..
+        F = [] #FInal states list in the form of indexes (int)
+        q0 = self.root.firstpos
+
+        Q.append(q0)
+        if contains_hashtag(q0):
+            F.append(Q.index(q0))
+        
+        while len(Q) - len(M) > 0:
+            #There exists one unmarked
+            #We take one of those
+            q = [i for i in Q if i not in M][0]
+            #Generating the delta dictionary for the new state
+            d.append({})
+            #We mark it
+            M.append(q)
+            #For each letter in the automata's alphabet
+            for a in V:
+                # Compute destination state ( d(q,a) = U )
+                U = []
+                #Compute U
+                #foreach position in state
+                for i in q:
+                    #if i has label a
+                    if self.followpos[i][0] == a:
+                        #We add the position to U's composition
+                        U = U + self.followpos[i][1]
+                U = sorted(list(set(U)))
+                #Checking if this is a valid state
+                if len(U) == 0:
+                    #No positions, skipping, it won't produce any new states ( also won't be final )
+                    continue
+                if U not in Q:
+                    Q.append(U)
+                    if contains_hashtag(U):
+                        F.append(Q.index(U))
+                #d(q,a) = U
+                d[Q.index(q)][a] = Q.index(U)
+        
+        return Dfa(Q,V,d,Q.index(q0),F)
+
+class Dfa:
+
+    def __init__(self,Q,V,d,q0,F):
+        self.Q = Q
+        self.V = V
+        self.d = d
+        self.q0 = q0
+        self.F = F
+
+
+    def write(self):
+        for i in range(len(self.Q)):
+            #Printing index, the delta fuunction for that transition and if it's final state
+            print(i,self.d[i],'F' if i in self.F else '')
