4f6f6ccf8426c07b0b7c78aff4adbbaf2bb93a6b
Younes Soleimani <YouS0>
2023-07-06 10:37:30 +0330

    RegexNode Class Created

--- Regex2NFA.py
+++ Regex2NFA.py
@@ -42,3 +42,166 @@ def valid_operations(regex):
                 return False
     return True
     
+class RegexNode:
+
+    @staticmethod
+    def trim_brackets(regex):
+        while regex[0] == '(' and regex[-1] == ')' and is_valid_regex(regex[1:-1]):
+            regex = regex[1:-1]
+        return regex
+    
+    @staticmethod
+    def is_concat(c):
+        return c == '(' or RegexNode.is_letter(c)
+    
+    @staticmethod
+    def is_letter(c):
+        return c in alphabet
+
+    def __init__(self, regex):
+        self.nullable = None
+        self.firstpos = []
+        self.lastpos = []
+        self.item = None
+        self.position = None
+        self.children = []
+
+        if DEBUG:
+            print('Current : '+regex)
+        #Check if it is leaf
+        if len(regex) == 1 and self.is_letter(regex):
+            #Leaf
+            self.item = regex
+            #Lambda checking
+            if use_lambda:
+                if self.item == lambda_symbol:
+                    self.nullable = True
+                else:
+                    self.nullable = False
+            else:
+                self.nullable = False
+            return
+        
+        #It is an internal node
+        #Finding the leftmost operators in all three
+        kleene = -1
+        or_operator = -1
+        concatenation = -1
+        i = 0
+
+        #Getting the rest of terms    
+        while i < len(regex):
+            if regex[i] == '(':
+                #Composed block
+                bracketing_level = 1
+                #Skipping the entire term
+                i+=1
+                while bracketing_level != 0 and i < len(regex):
+                    if regex[i] == '(':
+                        bracketing_level += 1
+                    if regex[i] == ')':
+                        bracketing_level -= 1
+                    i+=1
+            else:
+                #Going to the next char
+                i+=1
+            
+            #Found a concatenation in previous iteration
+            #And also it was the last element check if breaking
+            if i == len(regex):
+                break
+
+            #Testing if concatenation
+            if self.is_concat(regex[i]):
+                if concatenation == -1:
+                    concatenation = i
+                continue
+            #Testing for kleene
+            if regex[i] == '*':
+                if kleene == -1:
+                    kleene = i
+                continue
+            #Testing for or operator
+            if regex[i] == '|':
+                if or_operator == -1:
+                    or_operator = i
+        
+        #Setting the current operation by priority
+        if or_operator != -1:
+            #Found an or operation
+            self.item = '|'
+            self.children.append(RegexNode(self.trim_brackets(regex[:or_operator])))
+            self.children.append(RegexNode(self.trim_brackets(regex[(or_operator+1):])))
+        elif concatenation != -1:
+            #Found a concatenation
+            self.item = '.'
+            self.children.append(RegexNode(self.trim_brackets(regex[:concatenation])))
+            self.children.append(RegexNode(self.trim_brackets(regex[concatenation:])))
+        elif kleene != -1:
+            #Found a kleene
+            self.item = '*'
+            self.children.append(RegexNode(self.trim_brackets(regex[:kleene])))
+
+    def calc_functions(self, pos, followpos):
+        if self.is_letter(self.item):
+            #Is a leaf
+            self.firstpos = [pos]
+            self.lastpos = [pos]
+            self.position = pos
+            #Add the position in the followpos list
+            followpos.append([self.item,[]])
+            return pos+1
+        #Is an internal node
+        for child in self.children:
+            pos = child.calc_functions(pos, followpos)
+        #Calculate current functions
+
+        if self.item == '.':
+            #Is concatenation
+            #Firstpos
+            if self.children[0].nullable:
+                self.firstpos = sorted(list(set(self.children[0].firstpos + self.children[1].firstpos)))
+            else:
+                self.firstpos = deepcopy(self.children[0].firstpos)
+            #Lastpos
+            if self.children[1].nullable:
+                self.lastpos = sorted(list(set(self.children[0].lastpos + self.children[1].lastpos)))
+            else:
+                self.lastpos = deepcopy(self.children[1].lastpos)
+            #Nullable
+            self.nullable = self.children[0].nullable and self.children[1].nullable
+            #Followpos
+            for i in self.children[0].lastpos:
+                for j in self.children[1].firstpos:
+                    if j not in followpos[i][1]:
+                        followpos[i][1] = sorted(followpos[i][1] + [j])
+
+        elif self.item == '|':
+            #Is or operator
+            #Firstpos
+            self.firstpos = sorted(list(set(self.children[0].firstpos + self.children[1].firstpos)))
+            #Lastpos
+            self.lastpos = sorted(list(set(self.children[0].lastpos + self.children[1].lastpos)))
+            #Nullable
+            self.nullable = self.children[0].nullable or self.children[1].nullable
+
+        elif self.item == '*':
+            #Is kleene
+            #Firstpos
+            self.firstpos = deepcopy(self.children[0].firstpos)
+            #Lastpos
+            self.lastpos = deepcopy(self.children[0].lastpos)
+            #Nullable
+            self.nullable = True
+            #Followpos
+            for i in self.children[0].lastpos:
+                for j in self.children[0].firstpos:
+                    if j not in followpos[i][1]:
+                        followpos[i][1] = sorted(followpos[i][1] + [j])
+
+        return pos
+
+    def write_level(self, level):
+        print(str(level) + ' ' + self.item, self.firstpos, self.lastpos, self.nullable, '' if self.position == None else self.position)
+        for child in self.children:
+            child.write_level(level+1)
